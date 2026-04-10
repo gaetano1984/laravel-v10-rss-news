b46fba0206380d076b36123c98674d5e09a487ac
Gaetano Campanella <gaetano.campanella@novatag.it>
2026-04-10 10:49:37 +0200

    responsive

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,21 +8,16 @@
     </div> 
     <div class="row">
         @foreach($news as $k=>$new)
-            <div class="col-md-4">  
-                <div class="card">
+            <!-- <div class="col-sm-4"> -->
+            <div class="col-12 col-md-6 col-lg-4 mb-3">
+                <div class="card ">
                     <div class="card-body">
+                        {{ $k }}
                         <h5 class="card-title">{{$new['title']}}</h5>
                         <p class="card-text">{{$new['excerpt']}}</p>  
                     </div>
                 </div>
-            </div>            
-            @if($k%3==2)
-                </div>
-                <div class="row">
-                    &nbsp;
-                </div>
-                <div class="row">
-            @endif
+            </div>
         @endforeach
     </div>   
 @endsection
